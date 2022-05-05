71f60c19fb4d1d0ea89ccefdf0607a08638196c9
dicapisar <dicapisar@gmail.com>
2022-05-04 22:33:59 -0500

    Inicialitation Proyect

---
+++ src/main/java/com/dicapisar/coreManagerAPI/CoreManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.dicapisar.coreManagerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dicapisar/coreManagerAPI/CoreManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicapisar.coreManagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
