622df793f5dc43b68d62e257599192bbb3cdf425
dicapisar <dicapisar@gmail.com>
2022-07-25 18:33:21 -0500

    feature/item_controller: Creation of rest full for items: * adding validations for ItemCreateRequestDTO

--- src/main/java/com/dicapisar/coreManagerAPI/dtos/request/ItemCreateRequestDTO.java
+++ src/main/java/com/dicapisar/coreManagerAPI/dtos/request/ItemCreateRequestDTO.java
@@ -4,14 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class ItemCreateRequestDTO {
+    @NotEmpty(message = "The attribute 'name' must not be empty")
     private String name;
+
+    @NotEmpty(message = "The attribute 'price' must not be empty")
     private float price;
+
+    @NotEmpty(message = "The attribute 'brand' must not be empty")
     private Long brand;
+
+    @NotEmpty(message = "The attribute 'typeItem' must not be empty")
     private Long typeItem;
 }
